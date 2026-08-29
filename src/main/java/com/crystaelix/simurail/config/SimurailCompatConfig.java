ba990a8a919c05c6798ddf95e7efda67e378b313
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2026-08-30 01:18:47 +0800

    Increase default adhesion config and reduce default probe interval config

--- src/main/java/com/crystaelix/simurail/config/SimurailCompatConfig.java
+++ src/main/java/com/crystaelix/simurail/config/SimurailCompatConfig.java
@@ -7,17 +7,17 @@ public class SimurailCompatConfig extends SimurailBaseConfig {
 	public final ConfigGroup monorail = group(2, "monorail", "Monorail");
 	public final ConfigFloat monorailLateralMaxSpeedFactor = f(500, 0, Float.MAX_VALUE, "lateralMaxSpeedFactor", Units.acceleration, Comments.monorailLateralMaxSpeedFactor);
 	public final ConfigFloat monorailVerticalMaxSpeedFactor = f(100, 0, Float.MAX_VALUE, "verticalMaxSpeedFactor", Units.acceleration, Comments.monorailVerticalMaxSpeedFactor);
-	public final ConfigFloat monorailAdhesionFactor = f(15, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.monorailAdhesionFactor);
+	public final ConfigFloat monorailAdhesionFactor = f(24, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.monorailAdhesionFactor);
 
 	public final ConfigGroup narrow = group(2, "narrow", "Narrow");
 	public final ConfigFloat narrowLateralMaxSpeedFactor = f(25, 0, Float.MAX_VALUE, "lateralMaxSpeedFactor", Units.acceleration, Comments.narrowLateralMaxSpeedFactor);
 	public final ConfigFloat narrowVerticalMaxSpeedFactor = f(50, 0, Float.MAX_VALUE, "verticalMaxSpeedFactor", Units.acceleration, Comments.narrowVerticalMaxSpeedFactor);
-	public final ConfigFloat narrowAdhesionFactor = f(6, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.narrowAdhesionFactor);
+	public final ConfigFloat narrowAdhesionFactor = f(12, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.narrowAdhesionFactor);
 
 	public final ConfigGroup wide = group(2, "wide", "Wide");
 	public final ConfigFloat wideLateralMaxSpeedFactor = f(50, 0, Float.MAX_VALUE, "lateralMaxSpeedFactor", Units.acceleration, Comments.wideLateralMaxSpeedFactor);
 	public final ConfigFloat wideVerticalMaxSpeedFactor = f(50, 0, Float.MAX_VALUE, "lateralMaxSpeedFactor", Units.acceleration, Comments.wideVerticalMaxSpeedFactor);
-	public final ConfigFloat wideAdhesionFactor = f(6, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.wideAdhesionFactor);
+	public final ConfigFloat wideAdhesionFactor = f(12, 0, Float.MAX_VALUE, "adhesionFactor", Units.acceleration, Comments.wideAdhesionFactor);
 
 	@Override
 	public String getName() {
